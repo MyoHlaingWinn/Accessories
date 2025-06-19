de1ce5b906a7d256f254a6242fa65f1145a8169b
MyoHlaingWinn <myohlaingwinn.ict@gmail.com>
2025-06-19 14:51:33 +0630

    version1

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      <div className='text-3xl text-blue-300 bg-yellow-200'>Hello World</div>
+      <div>Nice to meet you</div>
+    </div>
+  )
+}
+
+export default page
