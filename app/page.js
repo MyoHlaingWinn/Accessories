4d447c800563d9eada622e66d31a0cd1570f4a29
MyoHlaingWinn <myohlaingwinn.ict@gmail.com>
2025-06-19 15:14:00 +0630

    version1.1

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ function page() {
     <div>
       <div className='text-3xl text-blue-300 bg-yellow-200'>Hello World</div>
       <div>Nice to meet you</div>
+      <img src='img/profile.jpg' alt='my photo' className='w-80'/>
     </div>
   )
 }
